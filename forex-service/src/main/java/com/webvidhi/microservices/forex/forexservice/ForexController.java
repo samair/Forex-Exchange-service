47a067251f887b081b7292634284e9da683ccdff
samair <samair@192.168.0.103>
2019-02-10 21:43:24 +0530

    First commit

---
+++ forex-service/src/main/java/com/webvidhi/microservices/forex/forexservice/ForexController.java
@@ -0,0 +1,31 @@
+package com.webvidhi.microservices.forex.forexservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.webvidhi.microservices.forex.forexservice.model.ExchangeValue;
+import com.webvidhi.microservices.forex.forexservice.model.ExchangeValueRepository;
+
+@RestController
+public class ForexController {
+	
+	@Autowired
+	private Environment environment;
+	
+	@Autowired
+	private ExchangeValueRepository repository;
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public ExchangeValue getExchangeValue(@PathVariable String from, @PathVariable String to) {
+		
+		ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
+		
+		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		return exchangeValue;
+	}
+	
+
+}
---
+++ forex-service/src/main/java/com/webvidhi/microservices/forex/forexservice/ForexServiceApplication.java
@@ -0,0 +1,14 @@
+package com.webvidhi.microservices.forex.forexservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForexServiceApplication.class, args);
+	}
+
+}
+
