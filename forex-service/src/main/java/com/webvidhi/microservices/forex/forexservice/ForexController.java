ae9bf4b4f02c1a5859612f69789d8e6a80161755
samair <samair@192.168.0.101>
2019-02-26 18:44:49 +0530

    Added eureka registration

--- forex-service/src/main/java/com/webvidhi/microservices/forex/forexservice/ForexController.java
+++ forex-service/src/main/java/com/webvidhi/microservices/forex/forexservice/ForexController.java
@@ -1,6 +1,8 @@
 package com.webvidhi.microservices.forex.forexservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.system.ApplicationPid;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.webvidhi.microservices.forex.forexservice.model.ExchangeValue;
 import com.webvidhi.microservices.forex.forexservice.model.ExchangeValueRepository;
 
+@EnableEurekaClient
 @RestController
 public class ForexController {
 	
@@ -25,6 +28,12 @@ public class ForexController {
 		
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
 		return exchangeValue;
+	}
+	
+	@GetMapping("/test")
+	public String test() {
+		//ApplicationPid pid = new ApplicationPid();
+		return "Ola from Forex service :" ;
 	}
 	
 
